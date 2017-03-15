c4e03b3b7b9a23f0f0eec03336dd3458f50e7766
iwbolling <iwbolling@gmail.com>
2017-03-15 17:08:14 -0400

    basic AppRouter and ViewController started

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -1,6 +1,48 @@
 // IMPORTS - BACKBONE
-import Backbone from 'backbone';
+import Backbone from "backbone";
 
 // IMPORTS - REACT
-import ReactDOM from 'react-dom'
-import React from 'react'
+import ReactDOM from "react-dom";
+import React from "react";
+
+// IMPORTS - VIEW CONTROLLER
+import {ViewController} from "./viewController.js";
+
+// APPROUTER
+const AppRouter = Backbone.router.extend({
+	initialize: function(){
+		Backbone.history.start()
+	},
+	routes: {
+		"" : "showWelcome",
+		"login" : "showLoginForm",
+		"register" : "showRegisterForm",
+		"lists" : "showAllLists",
+		"lists/single" : "showSingleList",
+		"lists/create" : "showCreateListForm",
+		"lists/edit" : "showEditListForm"
+	},
+	showWelcome: function(){
+
+	},
+	showLoginForm: function(){
+
+	},
+	showRegisterForm: function(){
+
+	},
+	showAllLists: function(){
+
+	},
+	showSingleList: function(){
+
+	},
+	showCreateListForm: function(){
+
+	},
+	showEditListForm: function(){
+
+	}
+});
+
+new AppRouter();
---
+++ Application/src/Application.Web/src-client/scripts/viewController.js
@@ -0,0 +1,5 @@
+// IMPORTS - REACT
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const ViewController = React.createClass({})
