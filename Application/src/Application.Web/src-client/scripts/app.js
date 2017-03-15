c9ed6acddfeb254fa29cbf145c2b53672eb60e38
iwbolling <iwbolling@gmail.com>
2017-03-15 16:34:24 -0400

    nuked app.js in prep for router

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -1,18 +1,6 @@
+// IMPORTS - BACKBONE
 import Backbone from 'backbone';
+
+// IMPORTS - REACT
 import ReactDOM from 'react-dom'
 import React from 'react'
-
-const SomeComponent = React.createClass({
-	render: function(){
-		return (
-			<div>
-				<h1> I &#10084; u<br/> Baby Shampu </h1>
-				<p><small>
-					you make my hair so soft and i know you will never make me cry.
-				</small></p>
-			</div>
-		)
-	}
-})
-
-ReactDOM.render(<SomeComponent/>, document.querySelector('#app-container'))
--- Application/src/Application.Web/src-client/scripts/views/view-singlelist.js
+++ Application/src/Application.Web/src-client/scripts/views/view-singlelist.js
@@ -1,3 +1,6 @@
+// IMPORTS - BACKBONE
+import Backbone from "backbone";
+
 // IMPORTS - REACT
 import React from "react";
 import ReactDOM from "react-dom";
