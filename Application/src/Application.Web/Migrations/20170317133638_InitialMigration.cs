647c51fd31e3876c7f4d2733fbb17f0bb7a7d7ea
JeffWeinreich <jeffweinreich@gmail.com>
2017-03-17 10:07:45 -0400

    test

--- Application/src/Application.Web/Migrations/20170317133638_InitialMigration.cs
+++ Application/src/Application.Web/Migrations/20170317133638_InitialMigration.cs
@@ -9,7 +9,7 @@ namespace Application.Web.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Users",
+                name: "Users", 
                 columns: table => new
                 {
                     Id = table.Column<string>(nullable: false),
