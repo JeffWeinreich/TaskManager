cbdd8fa4112e451070fc5f6e909ddc85a5ec9f75
iwbolling <iwbolling@gmail.com>
2017-03-22 15:07:17 -0400

    modified structure and styling of welcome page

--- Application/src/Application.Web/src-client/scripts/components/component-welcome.js
+++ Application/src/Application.Web/src-client/scripts/components/component-welcome.js
@@ -7,12 +7,12 @@ export const WelcomeComponent = React.createClass({
 	render: function(){
 		return (
 			<div className="component-welcome">
-	      <div className="welcome-header">
+	      {/* <div className="welcome-header">
 	        <div className="welcome-header_block">
 	          <h1>TaskMaster</h1>
 	          <h2>The convenient way to share to-do lists!</h2>
 	        </div>
-	      </div>
+	      </div> */}
 	      <div className="promo-blurb">
 	        <div className="promo-blurb_block">
 	          <h3>Now when you make a todo list, you can share it those who need it!</h3>
--- Application/src/Application.Web/src-client/scripts/views/view-welcome.js
+++ Application/src/Application.Web/src-client/scripts/views/view-welcome.js
@@ -10,7 +10,11 @@ export const WelcomeView = React.createClass({
 	render: function(){
 		return (
 			<div className="container">
-					<WelcomeComponent/>
+				<div className="page-header">
+          <h1>TaskMaster</h1>
+          <h2>The convenient way to share to-do lists</h2>
+        </div>
+				<WelcomeComponent/>
 			</div>
 		)
 	}
