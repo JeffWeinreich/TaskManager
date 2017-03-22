13d6a9eb3c2ae14d2d41a6cfad7cf4a8efb08adf
Avery Effa <averyheffa@gmail.com>
2017-03-22 10:44:45 -0400

    basic navbar and welcome styles

--- Application/src/Application.Web/src-client/scripts/components/component-navbar.js
+++ Application/src/Application.Web/src-client/scripts/components/component-navbar.js
@@ -3,11 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export const Navbar = React.createClass({
-  
+
   render: function(){
    return (
     <div className="nav-top columns-container">
-      <div className="nav-top_menu-button"><i className="fa fa-bars" aria-hidden="true"></i></div>
+      <div className="nav-top_menu-button">Menu Icon!</div>
         <div className="nav-top_right-buttons columns-container">
           <div className="right-buttons_sign-up">Sign Up</div>
           <div className="right-buttons_log-in">Log In</div>
