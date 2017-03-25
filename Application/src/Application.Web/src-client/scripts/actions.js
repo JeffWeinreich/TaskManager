b574fb07189b992f762e7a5b23af575101f3c431
Avery Effa <averyheffa@gmail.com>
2017-03-25 14:20:56 -0400

    form validation for login

--- Application/src/Application.Web/src-client/scripts/actions.js
+++ Application/src/Application.Web/src-client/scripts/actions.js
@@ -3,20 +3,28 @@ import Backbone from 'backbone'
 
 // IMPORTS - DATA FLOW
 import {STORE} from './store.js'
+import {UserModel} from './models/model-user.js'
 
 export const ACTIONS = {
 	setView: function(viewName){
 		STORE.setStore('currentView', viewName)
 	},
 	setAPIData: function(results){
-		STORE.setStore("dummyData",results)
+		STORE.setStore('dummyData',results)
+	},
+	loginUser: function(credsObj){
+	console.log(credsObj)
+	UserModel.logIn( credsObj.email , credsObj.password ).then(function(serverRes){
+		console.log(serverRes)
+		STORE.setStore('currentUser', serverRes)
+	})
 	},
 	routeTo: function(path){
 	window.location.hash = path
-},
-logUserOut: function(){
-UserModel.logOut().then(function(){
-	STORE.setStore('currentUser', {})
-})
-},
+	},
+	logUserOut: function(){
+		UserModel.logOut().then(function(){
+			STORE.setStore('currentUser', {})
+		})
+	}
 }
