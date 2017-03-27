c5c29747a34ce50ba6344b2b9caa1d98f7c58e7d
iwbolling <iwbolling@gmail.com>
2017-03-27 13:55:46 -0400

    fixed validation and submission in registration view

--- Application/src/Application.Web/src-client/scripts/components/component-form_registration.js
+++ Application/src/Application.Web/src-client/scripts/components/component-form_registration.js
@@ -8,7 +8,7 @@ import {ACTIONS} from "../actions.js";
 export const RegistrationComponent = React.createClass({
 	getInitialState: function(){
 		return {
-			emailErrorMsg: "false",
+			emailErrorMsg: "",
 			passwordErrorMsg: "",
 			confirmPwErrorMsg: ""
 		}
@@ -26,6 +26,12 @@ export const RegistrationComponent = React.createClass({
 			return /\d/.test(str);
 		};
 		let validationError = false;
+		this.setState({
+			emailErrorMsg : "",
+			passwordErrorMsg : "",
+			confirmPwErrorMsg : ""
+		});
+		let regObjToSave;
 		if (givenEmail.indexOf("@") === -1){
 			validationError = true;
 			this.setState({emailErrorMsg: emailErrorJSX});
@@ -38,12 +44,17 @@ export const RegistrationComponent = React.createClass({
 			validationError = true;
 			this.setState({confirmPwErrorMsg: confirmErrorJSX});
 		};
-		if (validationError = false){
+		if (validationError === false){
 			regObjToSave = {
 				email: givenEmail,
 				password: givenPassword
 			}
 			console.log(regObjToSave);
+			this.setState({
+				emailErrorMsg : "",
+				passwordErrorMsg : "",
+				confirmPwErrorMsg : ""
+			})
 			ACTIONS.registerNewUser(regObjToSave)
 		};
 	},
@@ -59,10 +70,13 @@ export const RegistrationComponent = React.createClass({
 				</div>
 				<div className="registration-container column-container">
 						<h3>Email</h3>
+						{this.state.emailErrorMsg}
 						<input type="text" className="form-control" name="emailField"/>
 						<h3>Password</h3>
+						{this.state.passwordErrorMsg}
 						<input type="password" className="form-control" name="passwordField"/>
 						<h3>Confirm Password</h3>
+						{this.state.confirmPwErrorMsg}
 						<input type="password" className="form-control" name="confirmPasswordField"/>
 				</div>
 				<div className="reg-submit reg-btn">
